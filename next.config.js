b816cc64c613c2a0cf2e62ed3abcc605f31748d5
Carlos Precioso <kalprestito@gmail.com>
2018-09-16 12:36:01 +0200

    Add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 const withTypescript = require("@zeit/next-typescript")
 
-module.exports = withTypescript()
+const { BUNDLE_ANALYZE } = process.env
+const withBundleAnalyzer =
+  BUNDLE_ANALYZE && require("@zeit/next-bundle-analyzer")
+
+module.exports = withTypescript(
+  BUNDLE_ANALYZE
+    ? withBundleAnalyzer({
+        analyzeServer: ["server", "both"].includes(BUNDLE_ANALYZE),
+        analyzeBrowser: ["browser", "both"].includes(BUNDLE_ANALYZE),
+        bundleAnalyzerConfig: {
+          server: {
+            analyzerMode: "static",
+            reportFilename: "../../bundles/server.html"
+          },
+          browser: {
+            analyzerMode: "static",
+            reportFilename: "../bundles/client.html"
+          }
+        }
+      })
+    : {}
+)
