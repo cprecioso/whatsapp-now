c89c5bd43e5f5724a4257a3e3a8a3172bb8d3547
Carlos Precioso <kalprestito@gmail.com>
2018-11-12 03:27:41 +0100

    Remove bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,24 +1,2 @@
 const withTypescript = require("@zeit/next-typescript")
-
-const { BUNDLE_ANALYZE } = process.env
-const withBundleAnalyzer =
-  BUNDLE_ANALYZE && require("@zeit/next-bundle-analyzer")
-
-module.exports = withTypescript(
-  BUNDLE_ANALYZE
-    ? withBundleAnalyzer({
-        analyzeServer: ["server", "both"].includes(BUNDLE_ANALYZE),
-        analyzeBrowser: ["browser", "both"].includes(BUNDLE_ANALYZE),
-        bundleAnalyzerConfig: {
-          server: {
-            analyzerMode: "static",
-            reportFilename: "../../bundles/server.html"
-          },
-          browser: {
-            analyzerMode: "static",
-            reportFilename: "../bundles/client.html"
-          }
-        }
-      })
-    : {}
-)
+module.exports = withTypescript()
